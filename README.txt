9ecba78119a8c4ea43d9e83b9842a64c6ec02ba2
vp2010602-debug <vp2010602@gmail.com>
2026-06-17 12:49:59 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,4 @@
+Total questions: 1290
+Upload all files and data folder to GitHub.
+Game ends only when other players lose/eliminate.
+Questions do not repeat within same room until exhausted.
---
+++ firebase.js
@@ -0,0 +1,3 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase, ref, set, update, onValue, get, push, serverTimestamp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+const firebaseConfig={apiKey:"AIzaSyBuNbEcsm_aiXIUXxsVq6vkMDBZNPexH88",authDomain:"bingovicky-ebad1.firebaseapp.com",databaseURL:"https://bingovicky-ebad1-default-rtdb.asia-southeast1.firebasedatabase.app",projectId:"bingovicky-ebad1",storageBucket:"bingovicky-ebad1.firebasestorage.app",messagingSenderId:"69770922572",appId:"1:69770922572:web:23509243d16bae088942e9",measurementId:"G-KTB33J64VQ"};const app=initializeApp(firebaseConfig);const db=getDatabase(app);window.FirebaseGame={db,ref,set,update,onValue,get,push,serverTimestamp};
